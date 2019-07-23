00acb4c5bacd69afab5d4bd65d467ff0573de93d
oleflaten <ole.flaten@hihm.no>
2019-07-23 17:05:24 +0200

    Updated pointer test.

--- tests.h
+++ tests.h
@@ -28,17 +28,22 @@ void modernPointers()
         Test *temp = new Test();
         temp->x = 40;
         temp->y += temp->x;
-//        delete temp;
+        temp->name[4] = 'G';
+        delete temp;
     }
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 
+    Test temp;
+    temp.name[3] = 'F';
+    std::cout << "Size of Test " << sizeof (temp) << std::endl;
+
     int precision = std::numeric_limits<double>::max_digits10;
     std::cout << std::setprecision(precision) << "Make and delete " << TEST_ROUNDS
               << " objects on heap with new: " << duration.count()*1000 << " ms" << std::endl;
 
-    //modern pointers
+    //unique ptr
     start = std::chrono::high_resolution_clock::now();
     for (int i{}; i < TEST_ROUNDS; i++) {
         std::unique_ptr<Test> temp = std::make_unique<Test>();
@@ -49,10 +54,32 @@ void modernPointers()
     end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double>  duration2 = end - start;
 
+    std::unique_ptr<Test> temp2 = std::make_unique<Test>();
+    std::cout << "Size of Test with make_unique: " << sizeof (*temp2) << std::endl;
+
     std::cout << std::setprecision(precision) << "Make and delete " << TEST_ROUNDS
               << " objects on heap with make_unique: " << duration2.count()*1000  << " ms" << std::endl;
 
     std::cout << std::setprecision(precision) << "Unique pointers / raw pointers: " <<
                  duration2.count() / duration.count() << std::endl;
 
+    //shared ptr
+    start = std::chrono::high_resolution_clock::now();
+    for (int i{}; i < TEST_ROUNDS; i++) {
+        std::shared_ptr<Test> temp = std::make_shared<Test>();
+        temp->x = 40;
+        temp->y += temp->x;
+    }
+
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double>  duration3 = end - start;
+
+    std::shared_ptr<Test> temp3 = std::make_shared<Test>();
+    std::cout << "Size of Test with make_shared: " << sizeof (*temp3) << std::endl;
+
+    std::cout << std::setprecision(precision) << "Make and delete " << TEST_ROUNDS
+              << " objects on heap with make_unique: " << duration3.count()*1000  << " ms" << std::endl;
+
+    std::cout << std::setprecision(precision) << "shared pointers / raw pointers: " <<
+                 duration3.count() / duration.count() << std::endl;
 }
